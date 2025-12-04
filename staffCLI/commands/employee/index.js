e9db33ea3eacf84afaa519f633a65e4950e3d513
BellaFred <156227097+BellaFred@users.noreply.github.com>
2025-12-03 20:31:21 -0600

    more commands and some changes to index to allow spaces within quotations

--- staffCLI/commands/employee/index.js
+++ staffCLI/commands/employee/index.js
@@ -4,9 +4,17 @@ import updatePetCommand from './updatePet.js';
 import sellPetCommand from './sellPet.js'; 
 import searchPetsCommand from './searchPets.js';
 import viewPetCommand from './viewPet.js';
+import searchOrdersCommand from './searchOrders.js';
+import viewOrderCommand from './viewOrder.js';
+import searchCustomersCommand from './searchCustomers.js';
+import viewCustomerCommand from './viewCustomer.js';
 
 export const employeeCommands = [
     updatePetCommand,
+    searchOrdersCommand,
+    viewOrderCommand,
+    searchCustomersCommand,
+    viewCustomerCommand,
     addPetCommand,
    deletePetCommand,
    sellPetCommand, 
